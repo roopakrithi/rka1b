f6997d7914576d953e982b70de3411c6faa0de76
roopakrithi <roopa.krithi@gmail.com>
2017-09-02 12:25:08 -0400

    Second attempt at assignment 1. Using RStudio to create a package directly (instead of usng "create")

---
+++ R/emu_function.R
@@ -0,0 +1,17 @@
+#' A function about emus
+#'
+#' What bird should you hug today? Find out here
+#' @param love Do you love emus? Defaults to TRUE
+#' @keywords bird
+#' @export
+#' @examples
+#' emu_function()
+
+emu_function <- function(love = T){
+  if (love == T) {
+    print ("Give a flightless bird a hug today.")
+  }
+  else {
+    print ("Hug a raptor. Wear protective gloves.")
+    }
+  }
